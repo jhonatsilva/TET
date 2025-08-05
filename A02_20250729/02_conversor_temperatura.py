03a6b73dc6dde953a9ef7c6e9cb76c004724e311
jhonatsilva <dasilva.jhonathan@icloud.com>
2025-08-04 22:02:24 -0300

    fix exercicios

--- A02_20250729/02_conversor_temperatura.py
+++
@@ -1,5 +0,0 @@
-l_ceusius = float(input("Digite o grau em ceusius: "))
-
-l_fahrenheit = (l_ceusius * (9/5)) + 32
-
-print("O valor de ", l_ceusius, " graus ceusius em fahrenheit eh: ", l_fahrenheit)
